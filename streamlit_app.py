d8834c6cbba3d1715c5de0ba5ab124d3777ab7e3
Yesid89 <33935369+Yesid89@users.noreply.github.com>
2026-03-04 16:44:59 +0000

    titulo español

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My nueva apliacion para Pruebas")
+st.title("🎈 Mi nueva aplicación para pruebas")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
